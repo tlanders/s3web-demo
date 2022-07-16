d1db1bba99d751da2351da7573830d4078137c4b
Troy Landers <tlanders@landersconsulting.biz>
2022-07-16 14:22:09 -0500

    added second www bucket

--- main.tf
+++ main.tf
@@ -15,12 +15,38 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# resource to create s3 bucket
-resource "aws_s3_bucket" "demo-bucket" {
+# resource to create redirect s3 bucket
+resource "aws_s3_bucket" "redirect-bucket" {
   bucket = "lci-demo-bucket"
 
   tags = {
-    name = "S3Bucket"
+    name = "redirect to www demo bucket"
   }
 }
 
+resource "aws_s3_bucket_website_configuration" "s3-redirect-config" {
+  bucket = aws_s3_bucket.redirect-bucket.bucket
+  redirect_all_requests_to {
+    host_name = aws_s3_bucket_website_configuration.s3-www-config.website_endpoint
+    #protocol = "https"
+  }
+}
+
+# resource to create www s3 bucket
+resource "aws_s3_bucket" "www-bucket" {
+  bucket = "www.lci-demo-bucket"
+
+  tags = {
+    name = "www demo bucket"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "s3-www-config" {
+  bucket = aws_s3_bucket.www-bucket.bucket
+  index_document {
+    suffix = "index.html"  
+  }
+  error_document {
+    key = "error.html"
+  }
+}
